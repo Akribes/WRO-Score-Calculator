e954d9e06698a92a34644fadf0effb32ca06c1a6
Mart Koster <the947thtokay@codingtime.nl>
2019-03-05 13:19:28 +0100

    Do calculations for smart lamps

--- main.js
+++ main.js
@@ -1,11 +1,65 @@
-let score = 0
+let scores = [0,0,0,0,0,0,0,0,0,0]
 
 const update = function () {
-  let inputs = document.getElementsByClassName('input')
+  scores[0] = Number(document.getElementById('npc1').value)
+  if (scores[0] !== 0) {
+    scores[0] += Number(document.getElementById('s1').value)
+    if (scores[0] === 20) {
+      scores[0] += 5
+    }
+  }
 
-  // TODO: add up all values
+  scores[1] = Number(document.getElementById('npc2').value)
+  if (scores[1] !== 0) {
+    scores[1] += Number(document.getElementById('s2').value)
+    if (scores[1] === 20) {
+      scores[1] += 5
+    }
+  }
 
-  document.getElementById('result').innerHTML = score
+  scores[2] = Number(document.getElementById('npc3').value)
+  if (scores[2] !== 0) {
+    scores[2] += Number(document.getElementById('s3').value)
+    if (scores[2] === 20) {
+      scores[2] += 5
+    }
+  }
+
+  scores[3] = Number(document.getElementById('npc4').value)
+  if (scores[3] !== 0) {
+    scores[3] += Number(document.getElementById('s4').value)
+    if (scores[3] === 20) {
+      scores[3] += 5
+    }
+  }
+
+  scores[4] = Number(document.getElementById('npc5').value)
+  if (scores[4] !== 0) {
+    scores[4] += Number(document.getElementById('s5').value)
+    if (scores[4] === 20) {
+      scores[4] += 5
+    }
+  }
+
+  scores[5] = Number(document.getElementById('npc6').value)
+  if (scores[5] !== 0) {
+    scores[5] += Number(document.getElementById('s6').value)
+    if (scores[5] === 20) {
+      scores[5] += 5
+    }
+  }
+
+  document.getElementById('result-1').innerHTML = String(scores[0])
+  document.getElementById('result-2').innerHTML = String(scores[1])
+  document.getElementById('result-3').innerHTML = String(scores[2])
+  document.getElementById('result-4').innerHTML = String(scores[3])
+  document.getElementById('result-5').innerHTML = String(scores[4])
+  document.getElementById('result-6').innerHTML = String(scores[5])
+  document.getElementById('result-b1').innerHTML = String(scores[6])
+  document.getElementById('result-b2').innerHTML = String(scores[7])
+  document.getElementById('result-walls').innerHTML = String(scores[8])
+  document.getElementById('result-end').innerHTML = String(scores[9])
+  document.getElementById('result').innerHTML = scores.reduce((partial_sum, a) => partial_sum + a)
 }
 
 function load() {
@@ -15,7 +69,9 @@ function load() {
     inputs[i].addEventListener('input', update, false)
   }
 
-  document.getElementById('result').innerHTML = score
+  document.getElementById('scoreForm').addEventListener('reset', update, false)
+
+  update()
 }
 
 document.addEventListener('DOMContentLoaded', load, false)
