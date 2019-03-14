24dc04a6b4cbab504ebafeb788b87073b06ccd9d
Mart Koster <the947thtokay@codingtime.nl>
2019-03-14 18:16:09 +0100

    Prevent double slashes from appearing in URL

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 'use strict'
 function updateLang () {
-  let newUrl = window.location.origin
+  let newUrl = window.location.origin + '/WRO-Score-Calculator'
   if (document.getElementById('lang').value !== '') {
-    newUrl += document.getElementById('lang').value + window.location.pathname.replace('WRO-Score-Calculator/', '')
+    newUrl += '/'
   }
+  newUrl += document.getElementById('lang').value + window.location.pathname.replace(/WRO-Score-Calculator(\/(nl))*/g, '')
+    .replace(/\/{2,}/g, '/')
   window.location.href = newUrl
 }
